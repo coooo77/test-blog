d462194fe6fb94db080e9f45f4c92d1e7a08f578
tzu lin chang <coooo77@hotmail.com>
2022-12-12 07:39:57 +0800

    app config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://coooo77.github.io',
+  trailingSlash: false,
+  baseUrl: '/test-blog/',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'coooo77', // Usually your GitHub org/user name.
+  projectName: 'test-blog', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
